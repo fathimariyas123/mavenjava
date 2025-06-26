fb7a8898418b6c8240f67805f0e6f3f3a330d8bd
Riyasameen <165645390+Riyasameen@users.noreply.github.com>
2025-06-26 09:08:06 +0530

    welcome

--- Fathima/src/main/java/simpleprograms/Print.java
+++ Fathima/src/main/java/simpleprograms/Print.java
@@ -6,6 +6,7 @@ public class Print {
 		// TODO Auto-generated method stub
 		System.out.println("Helo world");
 		System.out.println("java programming");
+		System.out.println("helo welcome java projects");
 
 	}
 
