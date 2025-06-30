b8386b58fba30852d033eb61b8401f24421065dd
Riyasameen <165645390+Riyasameen@users.noreply.github.com>
2025-06-30 19:17:53 +0530

    abstraction

---
+++ Fathima/src/main/java/abstractionsample/Car.java
@@ -0,0 +1,17 @@
+package abstractionsample;
+
+public class Car extends Vehicle {
+	public void start()
+	{
+		System.out.println("Using key and clatch");
+	}
+
+	public static void main(String[] args) {
+		Car C = new Car();
+		C.start();
+		C.display();
+		// TODO Auto-generated method stub
+
+	}
+
+}
